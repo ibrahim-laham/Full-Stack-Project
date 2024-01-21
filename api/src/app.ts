59bf24dae51891d7667a94711f272e92ee466c4b
ibrahim-laham <ibrahemlhm@yahoo.com>
2024-01-21 20:36:51 +0100

    deploy-c

--- api/src/app.ts
+++ api/src/app.ts
@@ -8,8 +8,25 @@ import usersRouter from "./routes/users";
 import ordersRouter from "./routes/orders";
 import apiErrorHandler from "./middlewares/apiErrorHandler";
 import { jwtStrategy } from "./config/passport";
+import path from "path";
+import { Request, Response } from "express";
 
 const app = Express();
+const _dirname = path.dirname("")
+const buildpath = path.join(_dirname, "../client/build")
+
+app.use(Express.static(buildpath))
+
+app.get("/", function(req: Request, res: Response)
+  {res.sendFile(
+    path.join(_dirname, "../client/build/index.html"),
+    function (error) {
+      if (error) {
+        res.status(500).send(error);
+      }
+    }
+  )}
+)
 
 app.use(Express.json());
 app.use(cors());
